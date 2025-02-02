9bd4d35a2af748193376254b778df5e59057a582
Georgy Pomytkin <georgy.pomytkin@yandex.ru>
2025-02-02 16:55:27 +0100

    Initial commit

---
+++ tests/test_main_page.py
@@ -0,0 +1,17 @@
+import pytest
+import allure
+
+from page_objects.main_page import MainPage
+from data import TestData
+
+
+class TestMainPageFaq:
+    @allure.title('Проверка раздела "Вопросы о важном"')
+    @pytest.mark.parametrize('question_number, expected_answer', TestData.test_data_expected_answer_faq)
+    def test_click_faq_expand_icons_text_is_expected(self, driver, question_number, expected_answer):
+        main_page = MainPage(driver)
+        main_page.scroll_to_faq_section()
+        main_page.wait_visibility_of_faq_questions(question_number)
+        main_page.click_on_faq_question(question_number)
+        main_page.wait_visibility_of_faq_answer(question_number)
+        assert main_page.get_displayed_text_from_faq_answer(question_number) == expected_answer
